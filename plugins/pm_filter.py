c8896acdb97fd5b50293dccd65de8fd6d45f7147
ABHINAND A S <abhinandunnikuttan04@gmail.com>
2022-08-11 02:46:54 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -364,7 +364,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
                     caption=f_caption,
                     protect_content=True if ident == "filep" else False 
                 )
-                await query.answer('Check Your PM 📥👀 \nI Just Send That Files For You 😻🤍'', show_alert=True)
+                await query.answer('Check Your PM 📥👀 \nI Just Send That Files For You 😻🤍', show_alert=True)
         except UserIsBlocked:
             await query.answer('Unblock the bot First 😤  \nThen Try Again ♻️!', show_alert=True)
         except PeerIdInvalid:
